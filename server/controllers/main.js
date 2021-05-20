56aa429bdc50bea647d3a6366e7152592be5304d
unknown <J.Domeikis@gmail.com>
2021-05-20 13:15:10 +0300

    userform

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -1,5 +1,26 @@
+const userDb = require('../schemas/userSchema')
+
 module.exports = {
     saveUser: (req, res) => {
+        const {
+            title,
+            age,
+            email,
+            password
+        } = req.body
+
+
+        const user = new userDb
+        user.title = title,
+        user.age = age,
+        user.email = email,
+        user.password = password,
+
+            user.save().then(res =>{
+                console.log(res)
+            })
+
+
         console.log(req.body)
         console.log('sup')
     }
--- server/router/router.js
+++ server/router/router.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/main')
+const userValidator = require('../middle/validateUser')
 
 // const validateRecipe = require('../middle/validateRecipe')
 // const validateReview = require('../middle/validateReview')
 //
-router.post('/adduser', controller.saveUser)
+router.post('/adduser', userValidator, controller.saveUser)
 // router.get('/getrecipes',  controller.getRecipes)
 // router.get('/singleRecipe/:id',  controller.getSingleRecipe)
 // router.post('/makeReview', validateReview,  controller.makeReview)
